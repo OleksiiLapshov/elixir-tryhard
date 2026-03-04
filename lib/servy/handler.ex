b6ec04f04b9de3b87a1eb589115a8e9370329325
Oleksii Lapshov <izraphail@gmail.com>
2026-03-04 22:58:13 +0200

    pattern matching exploration

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -16,30 +16,48 @@ defmodule Servy.Handler do
     |> List.first
     |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method, path: path, resp_body: "", status: nil }
   end
 
   def route(conv) do
     route(conv, conv.method, conv.path)
   end
 
-
   def route(conv, "GET", "/wildthings") do
-    %{ conv | resp_body: "Bears, Lions, Snakes"}
+    %{ conv | status: 200, resp_body: "Bears, Lions, Snakes"}
   end
 
   def route(conv, "GET", "/bears") do
-    %{ conv | resp_body: "Teddy, Baloo, Pooh"}
+    %{ conv | status: 200, resp_body: "Teddy, Baloo, Pooh"}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      501 => "Internal Server Error"
+    }[code]
   end
 
 end
@@ -57,7 +75,19 @@ response = Servy.Handler.handle(request)
 IO.puts response
 
 request = """
-GET /bears HTTP/1.1
+GET /bears/2 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /ufo HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
